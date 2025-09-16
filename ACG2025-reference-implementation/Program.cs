0882f525e8f1919f505a96d95a0f906656d80745
Yoka0014 <yuichiro.okashita.5z@stu.hosei.ac.jp>
2025-09-16 10:21:51 +0900

    first commit

---
+++ ACG2025-reference-implementation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ACG2025_reference_implementation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
